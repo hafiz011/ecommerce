97e7fcf808e0d12eac303fa834ac64590130a75b
hafiz011 <hrhafij8@gmail.com>
2025-11-06 18:03:49 +0600

    update details page product

--- obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ded632ed954ef97ac6791eb524a89f9743f426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5318e647028464389dafd35dbec95107cc1a32ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f6c31db8c3cdd1a40ee9ea91028c5def6c619c06
hafiz011 <hrhafij8@gmail.com>
2025-11-13 11:58:26 +0600

    update product card

--- obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
+++ obj/Debug/net8.0/ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97838b37a16e7bd3a7d26a07ed940653983ac5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53b52b683307271d38e2c00bf370b1ba950ed383")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

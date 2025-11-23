42e247f5341764b19ee80c33c5366321c4d05c22
hafiz011 <hrhafij8@gmail.com>
2025-11-23 14:18:12 +0600

    remove docker

---
+++ Models/Tenant.cs
@@ -0,0 +1,17 @@
+﻿namespace ecommerce.Models
+{
+    public class Tenant
+    {
+        public string TenantId { get; set; }
+        public string Name { get; set; }
+
+        // Default subdomain (tenant-1.your-saas.com)
+        public string SubDomain { get; set; }
+
+        // Client custom domain
+        public string? CustomDomain { get; set; }
+
+        public bool IsCustomDomainVerified { get; set; }
+    }
+
+}
